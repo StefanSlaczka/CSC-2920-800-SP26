a670ffe70c9952d0016b8c97dd828e22267f283c
StefanSlaczka <78507059+StefanSlaczka@users.noreply.github.com>
2026-02-12 17:24:13 -0500

    done

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,11 @@
+import "primereact/resources/themes/lara-light-indigo/theme.css"; // theme
+import "primereact/resources/primereact.min.css";                  // core styles
+import "primeicons/primeicons.css";                                // icons
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from "./App.tsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
